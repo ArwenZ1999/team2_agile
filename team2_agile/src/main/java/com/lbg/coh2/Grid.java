7eac845e803fb9d643598848513726a85bf0002f
yawenGit <arwen.zhang@outlook.com>
2022-10-21 12:29:30 +0100

    finish start postion

--- team2_agile/src/main/java/com/lbg/coh2/Grid.java
+++ team2_agile/src/main/java/com/lbg/coh2/Grid.java
@@ -4,20 +4,21 @@ import java.util.Random;
 
 public class Grid
 {
-	private int start_y = 0;
+	private int start_y=0;
 	private int treasure_x;
 	private int treasure_y;
 	private int gridSize;
 	private int current_x;
 	private int current_y;
 	
-	private int start_x = 0;
+	private int start_x;
 	public int getStart_x() {
 		return start_x;
 	}
 
 	public void setStart_x(int start_x) {
 		this.start_x = start_x;
+		this.current_x=start_x;
 	}
 
 	public int getStart_y() {
@@ -26,6 +27,7 @@ public class Grid
 
 	public void setStart_y(int start_y) {
 		this.start_y = start_y;
+		this.current_x=start_x;
 	}
 
 	public int getTreasure_x() {
@@ -68,26 +70,11 @@ public class Grid
 		this.current_y = current_y;
 	}
 	
-	public int startPosition()
-	{
-		Random rand = new Random();
-		int i = rand.nextInt(1);
-		if (i == 0)
-			startposition_x();
-		if (i == 1)
-			startposition_y();
-	}
-	
 	public int startposition_x()
 	{
 		Random rand = new Random();
 		start_x = rand.nextInt(gridSize);
 		return start_x;
 	}
-	public int startposition_y()
-	{
-		Random rand = new Random();
-		start_y = rand.nextInt(gridSize);
-		return start_y;
-	}
+	
 }

87047701508e0a007ad109604b43edb3aa64689a
Jack Lill <lill_jack@hotmail.com>
2022-10-21 12:05:29 +0100

    Added random start position

--- team2_agile/src/main/java/com/lbg/coh2/Grid.java
+++ team2_agile/src/main/java/com/lbg/coh2/Grid.java
@@ -1,53 +1,93 @@
 package com.lbg.coh2;
 
+import java.util.Random;
+
 public class Grid
 {
-	private int startx;
-	private int starty;
-	private int treasurex;
-	private int treasurey;
+	private int start_y = 0;
+	private int treasure_x;
+	private int treasure_y;
 	private int gridSize;
 	private int current_x;
 	private int current_y;
-	public int getStartx() {
-		return startx;
+	
+	private int start_x = 0;
+	public int getStart_x() {
+		return start_x;
 	}
-	public void setStartx(int startx) {
-		this.startx = startx;
+
+	public void setStart_x(int start_x) {
+		this.start_x = start_x;
 	}
-	public int getStarty() {
-		return starty;
+
+	public int getStart_y() {
+		return start_y;
 	}
-	public void setStarty(int starty) {
-		this.starty = starty;
+
+	public void setStart_y(int start_y) {
+		this.start_y = start_y;
 	}
-	public int getTreasurex() {
-		return treasurex;
+
+	public int getTreasure_x() {
+		return treasure_x;
 	}
-	public void setTreasurex(int treasurex) {
-		this.treasurex = treasurex;
+
+	public void setTreasure_x(int treasure_x) {
+		this.treasure_x = treasure_x;
 	}
-	public int getTreasurey() {
-		return treasurey;
+
+	public int getTreasure_y() {
+		return treasure_y;
 	}
-	public void setTreasurey(int treasurey) {
-		this.treasurey = treasurey;
+
+	public void setTreasure_y(int treasure_y) {
+		this.treasure_y = treasure_y;
 	}
+
 	public int getGridSize() {
 		return gridSize;
 	}
+
 	public void setGridSize(int gridSize) {
 		this.gridSize = gridSize;
 	}
-	public int startpostion_x()
-	{
-		//x
-		return 0;
+
+	public int getCurrent_x() {
+		return current_x;
 	}
-	public int startpostion_y()
-	{
-		return 0;
+
+	public void setCurrent_x(int current_x) {
+		this.current_x = current_x;
+	}
+
+	public int getCurrent_y() {
+		return current_y;
+	}
+
+	public void setCurrent_y(int current_y) {
+		this.current_y = current_y;
 	}
 	
+	public int startPosition()
+	{
+		Random rand = new Random();
+		int i = rand.nextInt(1);
+		if (i == 0)
+			startposition_x();
+		if (i == 1)
+			startposition_y();
+	}
 	
+	public int startposition_x()
+	{
+		Random rand = new Random();
+		start_x = rand.nextInt(gridSize);
+		return start_x;
+	}
+	public int startposition_y()
+	{
+		Random rand = new Random();
+		start_y = rand.nextInt(gridSize);
+		return start_y;
+	}
 }

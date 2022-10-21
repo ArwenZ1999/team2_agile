e952f11dba23869c3e154db773b55d8a2aa52b4f
Jack Lill <lill_jack@hotmail.com>
2022-10-21 11:46:13 +0100

    Added Grid class and data

---
+++ team2_agile/src/main/java/com/lbg/coh2/Grid.java
@@ -0,0 +1,43 @@
+package com.lbg.coh2;
+
+public class Grid
+{
+	private int startx;
+	private int starty;
+	private int treasurex;
+	private int treasurey;
+	private int gridSize;
+	
+	public int getStartx() {
+		return startx;
+	}
+	public void setStartx(int startx) {
+		this.startx = startx;
+	}
+	public int getStarty() {
+		return starty;
+	}
+	public void setStarty(int starty) {
+		this.starty = starty;
+	}
+	public int getTreasurex() {
+		return treasurex;
+	}
+	public void setTreasurex(int treasurex) {
+		this.treasurex = treasurex;
+	}
+	public int getTreasurey() {
+		return treasurey;
+	}
+	public void setTreasurey(int treasurey) {
+		this.treasurey = treasurey;
+	}
+	public int getGridSize() {
+		return gridSize;
+	}
+	public void setGridSize(int gridSize) {
+		this.gridSize = gridSize;
+	}
+	
+	
+}
